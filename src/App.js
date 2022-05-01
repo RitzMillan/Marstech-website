4543e583184264d3ec1b6254cd7151263afb41b6
Rishabh <ritzmillan@gmail.com>
2022-05-01 13:25:10 +0530

    Mobile view and Contact Us img

--- src/App.js
+++ src/App.js
@@ -1,47 +1,78 @@
 import "./App.scss";
-import React from "react";
-import {Howl, Howler} from 'howler';
-import ambient from './Assets/sounds/WinterRestlessness.mp3'
+import React, { useState, useEffect } from "react";
+import { Howl } from "howler";
+import ambient from "./Assets/sounds/WinterRestlessness.mp3";
 import Landing from "./Components/Landing";
+import rotate from "./Assets/icons/rotate.svg";
 
 function App() {
+
+
+  const [device, setDevice] = useState(
+    !!navigator.maxTouchPoints ? "mobile" : "computer"
+  );
+
+  const [orientation, setOrientation] = useState(
+    !navigator.maxTouchPoints ? "desktop" : !window.screen.orientation.angle ? "portrait" : "landscape"
+  );
 
   const amb = new Howl({
     src: [ambient],
     html5: true,
     volume: 0.5,
-    onplay: function(){
-      for(let i=1; i<=7; i++){
-        document.querySelector(`#stroke${i}`).classList.add('stroke')
-        document.querySelector(`#stroke${i}`).classList.remove('nostroke')
+    onplay: function () {
+      for (let i = 1; i <= 7; i++) {
+        document.querySelector(`#stroke${i}`).classList.add("stroke");
+        document.querySelector(`#stroke${i}`).classList.remove("nostroke");
       }
     },
-    onpause: function(){
-      for(let i=1; i<=7; i++){
-        document.querySelector(`#stroke${i}`).classList.add('nostroke')
-        document.querySelector(`#stroke${i}`).classList.remove('stroke')
+    onpause: function () {
+      for (let i = 1; i <= 7; i++) {
+        document.querySelector(`#stroke${i}`).classList.add("nostroke");
+        document.querySelector(`#stroke${i}`).classList.remove("stroke");
       }
-    }
+    },
   });
-
 
   let aid;
   function handleEnter() {
-    if(!amb.playing(aid) && !amb.playing()){
-      aid = amb.play()
-      console.log(aid,"if")
-    }
-    else if(amb.playing(aid)){
-        amb.pause(aid);
-      console.log(aid,"Pause(ID)")
-    }
-    else{
-      console.log(aid,"else")
-      amb.pause()
+    if (!amb.playing(aid) && !amb.playing()) {
+      aid = amb.play();
+      console.log(aid, "if");
+    } else if (amb.playing(aid)) {
+      amb.pause(aid);
+      console.log(aid, "Pause(ID)");
+    } else {
+      console.log(aid, "else");
+      amb.pause();
     }
   }
 
+  function handleDetect() {
+    setDevice(!!navigator.maxTouchPoints ? "mobile" : "computer");
+    setOrientation(
+      !navigator.maxTouchPoints
+        ? "desktop"
+        : !window.screen.orientation.angle
+        ? "portrait"
+        : "landscape"
+    );
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleDetect);
+
+    return () => {
+      window.addEventListener("resize", handleDetect);
+    };
+  }, []);
+
+  
+
   return (
+    <div>
+    {((device==="mobile" && orientation==="landscape") || (device==="computer" && orientation==="desktop"))
+    ?
     <div>
       <Landing />
       <div className="loader" onClick={handleEnter}>
@@ -54,8 +85,20 @@ function App() {
         <span id="stroke7"></span>
       </div>
 
-      <button className="enterButton" onClick={handleEnter}> Ready to Enter</button>
+      <button className="enterButton" onClick={handleEnter}>
+        Ready to Enter
+      </button>
     </div>
+    :
+    <div className="device-overlay-container">
+      <div className="rotation-overlay">
+        <h3>Please rotate your device</h3>
+        <img src={rotate} className="rotate-img" alt="rotate your device"></img>
+        <h4 className="helper-text">This website is specifically<br/>designed for Desktop</h4>
+      </div>
+    </div>
+    }
+  </div>
   );
 }
 
--- src/Components/Contact-page/Contact.js
+++ src/Components/Contact-page/Contact.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import "../../App.scss";
 import "./Contact.scss";
+import stencil from './artwork/stencil_film.svg'
+import cover from './artwork/cover_piece.svg'
+import astro from './artwork/astronaut.png'
 
 
 function Contact() {
@@ -8,7 +11,17 @@ function Contact() {
     <div>
         <div className='contact-grid-container'>
             <div className='contact-left-container'>
-
+              <div className='artwork'>
+                <div className='gradient'></div>
+                <div className='gradient-shadow'></div>
+                <img src={stencil} className="stencil" alt="stencil"></img>
+                <img src={cover} className="cover" alt="cover"></img>
+                <img src={astro} className="astro" alt="astronaut"></img>
+                <div className='text-box'>
+                  <p className='top-txt'>M A R S T E C H</p>
+                  <p className='bottom-txt'>P U L L I N G&nbsp;&nbsp;&nbsp;V A L U E&nbsp;&nbsp;&nbsp;F O R M&nbsp;&nbsp;&nbsp;T H E&nbsp;&nbsp;&nbsp;D A R K</p>
+                </div>
+              </div>
             </div>
             <div className='contact-right-container'>
                 <div className='form'>
